649e009980540589dd54336b489440a2d8896605
Devraj <devrajmandal5650@gmail.com>
2025-09-05 19:16:06 +0530

    Update Admin Page

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Add.css";
+import { assets } from "../../../src/assets/assets";
+const Add = () => {
+  return (
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" hidden required />
+        </div>
+        <div className="add-product-nameflex-col">
+          <p>Product name</p>
+          <input type="text" name="name" placeholder="Type here" />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product description</p>
+          <textarea
+            name="description"
+            rows="6"
+            placeholder="Write Content here"
+            required
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Desserts">Desserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product Price</p>
+            <input type="Number" name="price" placeholder="$20" />
+          </div>
+        </div>
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
+
+// 5:02:50
---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./List.css";
+const List = () => {
+  return <div>List</div>;
+};
+export default List;
