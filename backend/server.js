b36c5c6d1f74bbceb0f518558454b9290d4a4fba
Devraj Mandal <devrajmandal5650@gmail.com>
2025-09-08 16:15:46 +0530

    Update models and routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
 const app = express()
@@ -10,6 +11,7 @@ const port = 4000
 // middleware
 app.use(express.json());
 app.use(cors())
+app.use("/api/user",userRouter);
 
 // db connection
 connectDB();
