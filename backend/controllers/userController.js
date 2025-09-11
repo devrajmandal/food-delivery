bd0c4fa58a7635444229446feab8460c65a53678
Devraj Mandal <devrajmandal5650@gmail.com>
2025-09-11 13:33:18 +0530

    Integrate frontend with backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,27 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import validator from "validator"
 
 // login user
 const loginUser = async(req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false,message:"User doesn't exist"})
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
 
+        if(!isMatch){
+            return res.json({success:false,message:"Invalid credentials"})
+        }
+        const token=createToken(user._id);
+        res.json({success:true,token})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 const createToken = (id) => {
@@ -42,9 +58,12 @@ const registerUser = async(req,res) => {
         })
 
         const user = await newUser.save();
+        const token = createToken(user._id)
+        res.json({success:true,token})
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false,message:"Error"})
     }
 }
 
